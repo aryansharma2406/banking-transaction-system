ece4eec2e7cb4b279ca629d3333b6834b153af91
Aryan Sharma <aryans2409@gmail.com>
2026-03-24 09:22:01 +0530

    Added frontend UI

--- server.js
+++ server.js
@@ -9,12 +9,14 @@ const app = express();
 
 // MUST be before routes
 app.use(express.json());
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, "public")));
 const transactionRoutes = require("./routes/transactionRoutes");
 app.use("/api", transactionRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Banking Transaction System Running");
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
